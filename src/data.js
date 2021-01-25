58b83a249be86003b08befbc55a99f1c0b051d4a
Oscar Keirouz <oscar.keirouz@learnet.se>
2021-01-25 09:55:03 +0100

    React Uppgift 2

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Oscar',
+        username: 'Oscar Keirouz'
+    },
+    {
+        id: 2,
+        name: 'Hans',
+        username: 'Hans Lassie'
+    }
+];
+
+export default userList;
